4d8f408d1a88379447ebf3c47f6e65f0ce96e5ca
aidenhalili <78392788+aidenhalili@users.noreply.github.com>
2021-09-22 23:42:10 -0700

    added time change listener

--- app/src/main/java/com/example/walkitoff/MainActivity.java
+++ app/src/main/java/com/example/walkitoff/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.walkitoff;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
 
+    TimePicker timeSelected;
+
+    int hour, minute;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +20,18 @@ public class MainActivity extends AppCompatActivity {
         // TODO: read time picker input
 
         // initialize time picker
+        timeSelected = findViewById( R.id.timepicker );
+
+        // create anonymous class for time change listener
+        timeSelected.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int chosenHour, int chosenMinute) {
+
+                // save the chosen hour and minute
+                hour = chosenHour;
+                minute = chosenMinute;
+            }
+        });
+        // end anonymous class
     }
 }
