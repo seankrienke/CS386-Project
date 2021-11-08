d3e28c353b10ffb989af6499a6f366861f88ab75
aidenhalili <78392788+aidenhalili@users.noreply.github.com>
2021-11-08 15:14:44 -0700

    commented out the SoundFacade class

--- app/src/main/java/com/example/walkitoff/SoundFacade.java
+++ app/src/main/java/com/example/walkitoff/SoundFacade.java
@@ -2,6 +2,12 @@ package com.example.walkitoff;
 
 public class SoundFacade {
 
+    /**
+     * facade class so that the main activity can interact with the sound list class and sound class
+     * via this class only. The main activity only needs to input the level here and it can get an
+     * array to populate the sound dropdown menu and also use a string to get the integer value
+     * of a sound needed to input in the MediaPlayer.create method.
+     */
     private int level;
 
     public SoundFacade( int level ){
@@ -9,6 +15,11 @@ public class SoundFacade {
         this.level = level;
     }
 
+    /**
+     * gets an array of unlocked sounds to populate the sound dropdown menu
+     *
+     * @return array of the names of unlocked sounds
+     */
     public String[] getSoundArray(){
 
         Sound soundList = new Sound( level );
@@ -16,6 +27,13 @@ public class SoundFacade {
         return soundList.addAllUnlockedSounds();
     }
 
+    /**
+     * uses the string sound name to get the integer value needed for the MediaPlayer.create method
+     *
+     * @param soundName - String value chosen by the user in the dropdown menu
+     *
+     * @return integer value of sound
+     */
     public int getSound( String soundName ){
 
         return SoundName.getSound( soundName );
