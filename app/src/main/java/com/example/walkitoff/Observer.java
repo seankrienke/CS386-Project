3db7200659660922376005ff2a8ac41f77ca6962
aidenhalili <78392788+aidenhalili@users.noreply.github.com>
2021-11-07 21:22:47 -0700

    created observer class to observe distance and notify score

---
+++ app/src/main/java/com/example/walkitoff/Observer.java
@@ -0,0 +1,26 @@
+package com.example.walkitoff;
+
+public class Observer {
+
+    /**
+     * observer of the distance class, which will then notify the score class
+     * of any changes
+     */
+
+    public Observer(){
+
+    }
+
+    /**
+     * distance class will call this method to notify of the distance travelled
+     * in order to update the user score
+     * 
+     * @param distance
+     */
+    public void updateScore( int distance ){
+
+        // instantiate score object
+
+        // call update the score object with the distance
+    }
+}
