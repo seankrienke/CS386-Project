852cd94eaedb170d8e47b4b53cbe02711710de65
aidenhalili <78392788+aidenhalili@users.noreply.github.com>
2021-11-19 16:05:55 -0700

    added methods to AlarmList

--- app/src/main/java/com/example/walkitoff/AlarmList.java
+++ app/src/main/java/com/example/walkitoff/AlarmList.java
@@ -1,4 +1,76 @@
 package com.example.walkitoff;
 
 public class AlarmList {
+
+    private AlarmPreset[] alarmArray;
+
+    public static int size;
+
+    /**
+     * constructor
+     */
+    public AlarmList(){
+
+        size = 1;
+
+        alarmArray = new AlarmPreset[ size ];
+    }
+
+    /**
+     * adds alarm preset to array
+     *
+     * @param preset - alarm preset to add
+     */
+    public void addPreset( AlarmPreset preset ){
+
+        if( alarmArray[ 0 ] != null ){
+
+            resize();
+        }
+
+        alarmArray[ size - 1 ] = preset;
+
+    }
+
+    /**
+     * finds alarm preset by its label
+     *
+     * @param label - key for searching
+     *
+     * @return preset
+     */
+    public AlarmPreset findPreset( String label ){
+
+        int index;
+
+        for( index = 0; index < size; index++ ){
+
+            if( alarmArray[ index ].alarmLabel.equals( label ) ){
+
+                return alarmArray[ index ];
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * increments the size of the array
+     */
+    private void resize(){
+
+        int index;
+
+        AlarmPreset[] tempArray = new AlarmPreset[ size + 1 ];
+
+        for( index = 0; index < size; index++ ){
+
+            tempArray[ index ] = alarmArray[ index ];
+        }
+
+        alarmArray = tempArray;
+
+        size++;
+    }
+
 }
